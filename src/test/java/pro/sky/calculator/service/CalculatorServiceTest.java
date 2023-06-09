0e4abeaf062d2eeb922f3a55ccea134a0afff721
Дмитрий <dimakrjukov@gmail.com>
2023-06-09 14:46:24 +0200

    HappyEnd

--- src/test/java/pro/sky/calculator/service/CalculatorServiceTest.java
+++ src/test/java/pro/sky/calculator/service/CalculatorServiceTest.java
@@ -22,32 +22,32 @@ class CalculatorServiceTest {
     @Test
     @DisplayName("Проверка сложения")
     void plus() {
-        assertEquals("20 + 60 = 80", out.plus(20, 60));
-        assertNotEquals("20 + 0 = 200", out.plus(20, 0));
+        assertEquals(80, out.plus(20, 60));
+        assertNotEquals(200, out.plus(20, 0));
         assertNotNull(out.plus(20, 60));
     }
 
     @Test
     @DisplayName("Проверка вычитания")
     void minus() {
-        assertEquals("20 - 60 = -40", out.minus(20, 60));
-        assertNotEquals("20 - 0 = 2", out.minus(20, 0));
+        assertEquals( -40, out.minus(20, 60));
+        assertNotEquals(2, out.minus(20, 0));
         assertNotNull(out.plus(20, 60));
     }
 
     @Test
     @DisplayName("Проверка умножения")
     void multiply() {
-        assertEquals("20 * 60 = 1200", out.multiply(20, 60));
-        assertNotEquals("20 * 0 = 20", out.multiply(20, 0));
+        assertEquals(1200, out.multiply(20, 60));
+        assertNotEquals(20, out.multiply(20, 0));
         assertNotNull(out.plus(20, 60));
     }
 
     @Test
     @DisplayName("Проверка деления")
     void divide() {
-        assertEquals("40 / 20 = 2", out.divide(40, 20));
-        assertNotEquals("40 / 10 = 0", out.divide(40, 10));
+        assertEquals(2, out.divide(40, 20));
+        assertNotEquals(0, out.divide(40, 10));
         assertNotNull(out.plus(60, 20));
         assertThrows(IllegalArgumentException.class, () -> out.divide(40, 0));
     }
